5d14ff727d6ccfd9e210cd86d83219d1635f2b71
SmilnitskiyStas <stassmilnitskiy@gmail.com>
2023-05-30 23:23:58 +0300

    Add StyleCop file Add Architecture project

---
+++ ShiefCook/ShiefCook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShiefCook
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ShiefCook/ShiefCook/Starter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiefCook
+{
+    internal class Starter
+    {
+        public Starter()
+        {
+        }
+
+        public void Start()
+        {
+        }
+    }
+}
